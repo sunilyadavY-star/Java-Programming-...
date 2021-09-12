ce9805ab1d425d5d9c7e50395dfb819d064f5922
sunil yadav <56134580+sunilyadavY-star@users.noreply.github.com>
2021-09-12 20:22:19 +0530

    Add files via upload

---
+++ Hello World/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Sona");
+
+        int myFirastNumber = (10 +5) + ( 2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirastNumber *2;
+        int myTotal = myFirastNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+}
