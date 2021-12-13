b1333d07b79dee42ac333dd0b3577d36f2d1af04
sunil yadav <56134580+sunilyadavY-star@users.noreply.github.com>
2021-12-13 22:24:05 +0530

    Add files via upload

---
+++ Keyword/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
+
